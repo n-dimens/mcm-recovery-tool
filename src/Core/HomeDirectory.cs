e4cde14ab88ad8626228baeef8fa20610c46da44
Sergey Gorbunov <svgorbunov83@yandex.ru>
2019-07-05 17:51:35 +0300

    Logic moved to separate project Core

--- src/Core/HomeDirectory.cs
+++ src/Core/HomeDirectory.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MinecraftModsDeobfuscator.Domain {
-    class HomeDirectory : IHomeDirectory {
+    public class HomeDirectory : IHomeDirectory {
         private DirectoryInfo currentDirectory;
 
         private DirectoryInfo homeDirectory;
